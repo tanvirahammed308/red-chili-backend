f261447a033c8ccf456da87d1e0a3efe0fd77995
Tanvir <imtanvir.email@gmail.com>
2026-08-02 19:45:29 +0600

    fixed auth routes issue

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { createPaymentIntent } from "../controllers/payment.controller.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
-
+import authMiddleware from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.post(
